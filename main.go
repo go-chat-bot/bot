4daeed81b0ad59a20effc489763729f54d063862
Fábio Gomes <fabioxgn@gmail.com>
2013-12-31 17:43:11 -0200

    Removed commented code and translated constante that was in portuguese

--- main.go
+++ main.go
@@ -7,23 +7,22 @@ import (
 )
 
 const (
-	CANAL = "#lightirc"
+	CHANNEL = "#lightirc"
 )
 
 func main() {
 	irccon := irc.IRC("go-bot", "go-bot")
-	//irccon.VerboseCallbackHandler = true
 	irccon.UseTLS = true
 	err := irccon.Connect("irc.freenode.net:7000")
 	if err != nil {
 		log.Fatal(err)
 	}
 	irccon.AddCallback("001", func(e *irc.Event) {
-		irccon.Join(CANAL)
+		irccon.Join(CHANNEL)
 	})
 
 	irccon.AddCallback("366", func(e *irc.Event) {
-		irccon.Privmsg(CANAL, "Hi there.\n")
+		irccon.Privmsg(CHANNEL, "Hi there.\n")
 	})
 
 	irccon.AddCallback("PRIVMSG", func(e *irc.Event) {
