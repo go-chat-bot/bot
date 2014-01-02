965caf0f344107f02c8ede2389f578dad2b23b20
Fábio Gomes <fabioxgn@gmail.com>
2014-01-02 21:59:45 -0200

    Fixed StrAfter which was panicking when passing an empty string

--- string_utils.go
+++ string_utils.go
@@ -5,5 +5,8 @@ import (
 )
 
 func StrAfter(s, sep string) string {
+	if !strings.Contains(s, sep) {
+		return ""
+	}
 	return s[strings.Index(s, sep)+len(sep):]
 }
--- string_utils_test.go
+++ string_utils_test.go
@@ -12,3 +12,21 @@ func TestString(t *testing.T) {
 		t.Errorf("Want %v got %v", want, got)
 	}
 }
+
+func TestSeparatorNotFound(t *testing.T) {
+	sep := "!go-bot"
+	want := ""
+	got := StrAfter("all your base are belong to us", sep)
+	if got != want {
+		t.Errorf("Expected '%v' got '%v'", want, got)
+	}
+}
+
+func TestEmptyString(t *testing.T) {
+	sep := "!go-bot"
+	want := ""
+	got := StrAfter("", sep)
+	if got != want {
+		t.Errorf("Expected '%v' got '%v'", want, got)
+	}
+}
