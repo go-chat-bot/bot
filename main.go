edc8d1d92b8c1c92c0249a70ba3389a8a693dc48
Nom4d3 <nom4d3@gmail.com>
2014-02-04 15:40:46 -0200

    bug fixed: any message were parsed as a command

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/thoj/go-ircevent"
 	"log"
 	"os"
+	"strings"
 )
 
 const (
@@ -24,11 +25,20 @@ func onPRIVMSG(e *irc.Event) {
 		channel = e.Nick
 		args = e.Message
 	} else {
-		args = StrAfter(e.Message, config.Cmd)
+		// args = StrAfter(e.Message, config.Cmd)
+		// Test if the first word is the command sintax
+		x := strings.SplitN(e.Message, " ", 2)
+		if x[0] == config.Cmd {
+			// It's a command
+			args = x[1]
+			cmd := commands.Parse(args)
+			commands.HandleCmd(cmd, channel, irccon.Privmsg)
+		} else {
+			args = x[0]
+			// It's not a command
+			// Test for passive commands (parse url, etc) ?
+		}
 	}
-
-	cmd := commands.Parse(args)
-	commands.HandleCmd(cmd, channel, irccon.Privmsg)
 }
 
 func connect() {
@@ -58,7 +68,7 @@ func readConfig() {
 		panic(err)
 	}
 	config.Read(file)
-	fmt.Printf("%v", config)
+	fmt.Printf("%v\n", config)
 }
 
 func main() {
