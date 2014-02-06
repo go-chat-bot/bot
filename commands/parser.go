ff849e0c8d944cd16da7e8c1a3934668018fff30
Nom4d3 <nom4d3@gmail.com>
2014-02-06 16:07:17 -0200

    Huge revamp of the Parser module to understand and give better returns for all kind of commands

--- commands/parser.go
+++ commands/parser.go
@@ -2,24 +2,54 @@ package commands
 
 import (
 	"strings"
+	"regexp"
+	"log"
 )
 
 // Command is a struct which separates the user's input for easier handling of commands
 type Command struct {
-	Raw     string   // Raw is full string passed to the command
-	Command string   // Command is the first argument passed to the bot
-	Args    []string // Args is the remaining arguments after the command
+	Raw     		string   	// Raw is full string passed to the command
+	IsCommand 	bool   		// Confirmation if this is a command or just a regular message
+	Message 		string   	// Full string without the prefix
+	Command 		string   	// Command is the first argument passed to the bot
+	FullArg 		string 		// Full argument as a single string
+	Args    		[]string 	// Arguments as array
 }
 
 // Parse the arguments returning the Command to execute and the arguments passed to it
-func Parse(c string) *Command {
+func Parse(c string, prefix string) *Command {
 	cmd := &Command{Raw: c}
+	c = strings.TrimSpace(c)
+	cmd.IsCommand = strings.HasPrefix(c, prefix)
 
-	values := strings.SplitN(strings.Trim(c, " "), " ", 2)
+	// we can stop here if no prefix is detected
+	if !cmd.IsCommand {
+		cmd.Message = c
+		return cmd
+	}
 
-	cmd.Command = values[0]
-	if len(values) > 1 {
-		cmd.Args = strings.Split(values[1], " ")
+	// Trim the prefix and extra spaces
+	cmd.Message = strings.TrimPrefix(c, prefix)
+	cmd.Message = strings.TrimSpace(cmd.Message)
+
+	// check if we have the command and not only the prefix
+	cmd.IsCommand = cmd.Message != ""
+	if !cmd.IsCommand {
+		return cmd
+	}
+
+	// get the command
+	pieces := strings.SplitN(cmd.Message, " ", 2)
+	cmd.Command = pieces[0]
+
+	// get the arguments and remove extra spaces
+	if len(pieces) > 1 {
+		reg, err := regexp.Compile("\\s+")
+		if err != nil {
+         log.Fatal(err)
+      }
+		cmd.FullArg = reg.ReplaceAllString(strings.TrimSpace(pieces[1]), " ")
+		cmd.Args = strings.Split(cmd.FullArg, " ")
 	}
 
 	return cmd
