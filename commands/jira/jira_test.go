e4f2e0914ed79ccd598549c66abb252fa571091c
Roberto Schneiders <roberto.schneiders@gmail.com>
2014-05-23 18:55:26 -0300

    Changes the Jira command. This command now ignores other bots messages.

--- commands/jira/jira_test.go
+++ commands/jira/jira_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestJira(t *testing.T) {
 	url = "https://monde-sistemas.atlassian.net/browse/"
-
+	nick := "user"
 	Convey("Given a text", t, func() {
 		Convey("When the text does not match a jira issue syntax", func() {
 
-			s, err := getIssueURL("My name is go-bot, I am awesome.")
+			s, err := getIssueURL(nick, "My name is go-bot, I am awesome.")
 
 			So(err, ShouldBeNil)
 			So(s, ShouldEqual, "")
@@ -21,7 +21,7 @@ func TestJira(t *testing.T) {
 		Convey("When the text match a jira issue syntax", func() {
 			text := "My name is go-bot, I am awesome. MON-965"
 
-			s, err := getIssueURL(text)
+			s, err := getIssueURL(nick, text)
 
 			So(err, ShouldBeNil)
 			So(s, ShouldEqual, fmt.Sprintf("%s%s", url, "MON-965"))
@@ -30,7 +30,7 @@ func TestJira(t *testing.T) {
 		Convey("When the text has a jira issue in the midle of a word", func() {
 			text := "My name is goBOT-123"
 
-			s, err := getIssueURL(text)
+			s, err := getIssueURL(nick, text)
 
 			So(err, ShouldBeNil)
 			So(s, ShouldEqual, "")
@@ -39,7 +39,17 @@ func TestJira(t *testing.T) {
 		Convey("When the text has a jira issue syntax with only two numbers", func() {
 			text := "BOT-12"
 
-			s, err := getIssueURL(text)
+			s, err := getIssueURL(nick, text)
+
+			So(err, ShouldBeNil)
+			So(s, ShouldEqual, fmt.Sprintf("%s%s", url, "BOT-12"))
+		})
+	})
+	Convey("Given a nick", t, func() {
+		Convey("When the nick contains the word 'bot'", func() {
+			text := "BOT-122"
+
+			s, err := getIssueURL("somebot", text)
 
 			So(err, ShouldBeNil)
 			So(s, ShouldEqual, "")
