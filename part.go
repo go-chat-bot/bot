3057d473987e5ee7444f6e411479ab274aca98b1
Fábio Gomes <fabioxgn@gmail.com>
2014-09-20 03:02:46 +0000

    Removes useless check Split always return an array with len > 0

--- part.go
+++ part.go
@@ -14,15 +14,10 @@ const (
 func part(c *Cmd, channel, senderNick string, conn ircConnection) {
 	for _, configChannel := range config.Channels {
 		channelName := strings.Split(configChannel, " ")
-		if len(channelName) == 0 {
-			return
-		}
-
 		if strings.EqualFold(channelName[0], channel) {
 			conn.Privmsg(channel, partNotAllowed)
 			return
 		}
-
 	}
 
 	conn.Privmsg(channel, partMessage)
