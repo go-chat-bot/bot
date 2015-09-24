f7e014f7d37d8b62f4954fd84c91217907ef1394
Fábio Gomes <fabioxgn@gmail.com>
2015-09-24 00:04:58 -0300

    Removes .travis.yml

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "mode: set" > acc.out
-for Dir in $(find . -type d ); 
+for Dir in $(find . -type d );
 do
 	if ls $Dir/*.go &> /dev/null;
 	then
@@ -11,17 +11,17 @@ do
 		then
     		if [ -f profile.out ]
     		then
-        		cat profile.out | grep -v "mode: set" >> acc.out 
+        		cat profile.out | grep -v "mode: set" >> acc.out
     		fi
     	else
     		exit 1
-    	fi	
+    	fi
     fi
 done
 if [ -n "$GOVERALLS_TOKEN" ]
 then
     $HOME/gopath/bin/goveralls -coverprofile=acc.out -repotoken $GOVERALLS_TOKEN
-fi	
+fi
 
 rm -rf ./profile.out
 rm -rf ./acc.out
