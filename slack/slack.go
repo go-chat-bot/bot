1c054ad968dafba910565ff11b0e67ae6584ce3b
Fábio Gomes <fabioxgn@gmail.com>
2016-05-13 22:39:01 -0300

    Fix bot replying to own messages on slack (#30)
    Also doesn't process old channel messages upon login

--- slack/slack.go
+++ slack/slack.go
@@ -12,7 +12,8 @@ var (
 	rtm *slack.RTM
 	api *slack.Client
 
-	params = slack.PostMessageParameters{AsUser: true}
+	params    = slack.PostMessageParameters{AsUser: true}
+	botUserID = ""
 )
 
 func responseHandler(target string, message string, sender *bot.User) {
@@ -27,6 +28,19 @@ func extractUser(userID string) *bot.User {
 		return &bot.User{Nick: userID}
 	}
 	return &bot.User{Nick: slackUser.Name, RealName: slackUser.Profile.RealName}
+}
+
+func readBotInfo(api *slack.Client) {
+	info, err := api.AuthTest()
+	if err != nil {
+		fmt.Printf("Error calling AuthTest: %s\n", err)
+		return
+	}
+	botUserID = info.UserID
+}
+
+func ownMessage(UserID string) bool {
+	return botUserID == UserID
 }
 
 // Run connects to slack RTM API using the provided token
@@ -46,8 +60,12 @@ Loop:
 		select {
 		case msg := <-rtm.IncomingEvents:
 			switch ev := msg.Data.(type) {
+			case *slack.HelloEvent:
+				readBotInfo(api)
 			case *slack.MessageEvent:
-				b.MessageReceived(ev.Channel, ev.Text, extractUser(ev.User))
+				if !ev.Hidden && !ownMessage(ev.User) {
+					b.MessageReceived(ev.Channel, ev.Text, extractUser(ev.User))
+				}
 
 			case *slack.RTMError:
 				fmt.Printf("Error: %s\n", ev.Error())
