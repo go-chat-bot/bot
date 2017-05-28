go-chat-bot/bot

commit 562d4c2fd56ce7ecaa3b860e25846cfccdaa4920

Support for multiple periodic commands (#50)

bot.go
@@ -42,18 +42,20 @@ func New(h *Handlers) *Bot {
 
 func (b *Bot) startPeriodicCommands() {
 	for _, config := range periodicCommands {
-		b.cron.AddFunc(config.CronSpec, func() {
-			for _, channel := range config.Channels {
-				message, err := config.CmdFunc(channel)
-				if err != nil {
-					log.Print("Periodic command failed ", err)
-				} else if message != "" {
-					b.handlers.Response(channel, message, nil)
+		go func(config PeriodicConfig) {
+			b.cron.AddFunc(config.CronSpec, func() {
+				for _, channel := range config.Channels {
+					message, err := config.CmdFunc(channel)
+					if err != nil {
+						log.Print("Periodic command failed ", err)
+					} else if message != "" {
+						b.handlers.Response(channel, message, nil)
+					}
 				}
-			}
-		})
+			})
+		}(config)
 	}
-	if len(b.cron.Entries()) == 1 {
+	if len(periodicCommands) > 0 {
 		b.cron.Start()
 	}
 }

cmd_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -41,6 +42,9 @@ func TestPeriodicCommands(t *testing.T) {
 		})
 	b := New(&Handlers{Response: responseHandler})
 
+	// Give a second for the crons to be registered
+	time.Sleep(time.Second)
+
 	entries := b.cron.Entries()
 	if len(entries) != 1 {
 		t.Fatal("Should have one cron job entry")
@@ -59,6 +63,51 @@ func TestPeriodicCommands(t *testing.T) {
 	}
 }
 
+func TestMultiplePeriodicCommands(t *testing.T) {
+	resetResponses()
+	resetRegisteredPeriodicCommands()
+	RegisterPeriodicCommand("morning",
+		PeriodicConfig{
+			CronSpec: "0 0 08 * * mon-fri",
+			Channels: []string{"#channel"},
+			CmdFunc:  func(channel string) (string, error) { return "ok_morning " + channel, nil },
+		})
+	RegisterPeriodicCommand("afternoon",
+		PeriodicConfig{
+			CronSpec: "0 0 12 * * mon-fri",
+			Channels: []string{"#channel"},
+			CmdFunc:  func(channel string) (string, error) { return "ok_afternoon " + channel, nil },
+		})
+	b := New(&Handlers{Response: responseHandler})
+
+	// Give a second for the crons to be registered
+	time.Sleep(time.Second)
+
+	entries := b.cron.Entries()
+	if len(entries) != 2 {
+		t.Fatal("Should have 2 cron job entries")
+	}
+	if entries[0].Next.Hour() != 8 {
+		t.Fatal("First cron job should be scheduled for 8am")
+	}
+	if entries[1].Next.Hour() != 12 {
+		t.Fatal("Second cron job should be schedule for 12am")
+	}
+
+	entries[0].Job.Run()
+	entries[1].Job.Run()
+
+	if len(replies) != 2 {
+		t.Fatal("Should have two replies in the channel")
+	}
+	if replies[0] != "ok_morning #channel" {
+		t.Fatal("Invalid reply in first cron job")
+	}
+	if replies[1] != "ok_afternoon #channel" {
+		t.Fatal("Invalid reply in second cron job")
+	}
+}
+
 func TestErroredPeriodicCommand(t *testing.T) {
 	resetResponses()
 	resetRegisteredPeriodicCommands()
@@ -69,6 +118,9 @@ func TestErroredPeriodicCommand(t *testing.T) {
 			CmdFunc:  func(channel string) (string, error) { return "bug", errors.New("error") },
 		})
 	b := New(&Handlers{Response: responseHandler})
+
+	// Give a second for the crons to be registered
+	time.Sleep(time.Second)
 
 	entries := b.cron.Entries()
 

commit 9ee3c5e757762fd55bdb6f828c9bcaf92305d592

Fix race condition introduced in PR #50

bot.go
@@ -42,7 +42,7 @@ func New(h *Handlers) *Bot {
 
 func (b *Bot) startPeriodicCommands() {
 	for _, config := range periodicCommands {
-		go func(config PeriodicConfig) {
+		func(b *Bot, config PeriodicConfig) {
 			b.cron.AddFunc(config.CronSpec, func() {
 				for _, channel := range config.Channels {
 					message, err := config.CmdFunc(channel)
@@ -53,9 +53,9 @@ func (b *Bot) startPeriodicCommands() {
 					}
 				}
 			})
-		}(config)
+		}(b, config)
 	}
-	if len(periodicCommands) > 0 {
+	if len(b.cron.Entries()) > 0 {
 		b.cron.Start()
 	}
 }

cmd_test.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"testing"
-	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -42,9 +41,6 @@ func TestPeriodicCommands(t *testing.T) {
 		})
 	b := New(&Handlers{Response: responseHandler})
 
-	// Give a second for the crons to be registered
-	time.Sleep(time.Second)
-
 	entries := b.cron.Entries()
 	if len(entries) != 1 {
 		t.Fatal("Should have one cron job entry")
@@ -79,9 +75,6 @@ func TestMultiplePeriodicCommands(t *testing.T) {
 			CmdFunc:  func(channel string) (string, error) { return "ok_afternoon " + channel, nil },
 		})
 	b := New(&Handlers{Response: responseHandler})
-
-	// Give a second for the crons to be registered
-	time.Sleep(time.Second)
 
 	entries := b.cron.Entries()
 	if len(entries) != 2 {
@@ -118,9 +111,6 @@ func TestErroredPeriodicCommand(t *testing.T) {
 			CmdFunc:  func(channel string) (string, error) { return "bug", errors.New("error") },
 		})
 	b := New(&Handlers{Response: responseHandler})
-
-	// Give a second for the crons to be registered
-	time.Sleep(time.Second)
 
 	entries := b.cron.Entries()
 