a09ec3e820d6ae30109b2b7c686773aa956ccd60
Fabio Gomes <fabioxgn@gmail.com>
2017-07-19 18:06:53 +0000

    Removes goconvey from irc tests

--- irc/irc_test.go
+++ irc/irc_test.go
@@ -2,8 +2,6 @@ package irc
 
 import (
 	"testing"
-
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 const (
@@ -11,13 +9,7 @@ const (
 )
 
 func TestGetServerName(t *testing.T) {
-	Convey("Given server name", t, func() {
-		Convey("When there is no port specified", func() {
-			So(getServerName(serverName), ShouldEqual, serverName)
-		})
-
-		Convey("When there is a port specified", func() {
-			So(getServerName(serverName+":6667"), ShouldEqual, serverName)
-		})
-	})
+	if getServerName(serverName+":6667") != serverName {
+		t.Fatal("Port should be removed from server name")
+	}
 }
