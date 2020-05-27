ef71c72a524ae1242a47163d87b52dda69583bf6
Marc Pittinsky <marclane@live.unc.edu>
2020-05-27 14:14:14 -0400

    Parser: Use Fields to account for all unicode white spaces (#126)
    strings.SplitN was only matching U+0020 for splitting command and args
    Use strings.FieldsFunc to check unicode.IsSpace to account for all unicode white spaces
    
    reference: https://golang.org/pkg/strings/#FieldsFunc

--- parser.go
+++ parser.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"regexp"
 	"strings"
+	"unicode"
 
 	"github.com/mattn/go-shellwords"
 	unidecode "github.com/mozillazg/go-unidecode"
@@ -33,8 +34,17 @@ func parse(m *Message, channel *ChannelData, user *User) (*Cmd, error) {
 		return nil, nil
 	}
 
+	firstOccurrence := true
+	firstUnicodeSpace := func(c rune) bool {
+		isFirstSpace := unicode.IsSpace(c) && firstOccurrence
+		if isFirstSpace {
+			firstOccurrence = false
+		}
+		return isFirstSpace
+	}
+
 	// get the command
-	pieces := strings.SplitN(c.Message, " ", 2)
+	pieces := strings.FieldsFunc(c.Message, firstUnicodeSpace)
 	c.Command = strings.ToLower(unidecode.Unidecode(pieces[0]))
 
 	if len(pieces) > 1 {
